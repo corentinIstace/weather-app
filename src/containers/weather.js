235cad18466331e1eee8dbe730f62266e7cc8d5f
corentinIstace <corentinistace1@gmail.com>
2021-11-26 16:39:12 +0100

    adding showCity and design it

--- src/containers/weather.js
+++ src/containers/weather.js
@@ -17,6 +17,12 @@ export const Weather = () => {
     return (
         <Container>
             <Form error = {error} setError= {setError} setWeather = {setWeather}></Form>
+            {weather &&
+            <ShowCity>
+                <h2>Prévisions météo pour les 3 prochains jours à :</h2>
+                <h1>{weather.location.name}</h1>
+            </ShowCity>
+            }
             <Display>
             {weather &&
                 weather.forecast.forecastday.map((weatherDay) => {
@@ -24,8 +30,8 @@ export const Weather = () => {
                     key= {weatherDay.date} 
                     date={weatherDay.date} 
                     condition={weatherDay.day.condition}
-                    minTemp={weatherDay.day.mintemp_c}
-                    maxTemp={weatherDay.day.maxtemp_c}
+                    minTemp={Math.round(weatherDay.day.mintemp_c)}
+                    maxTemp={Math.round(weatherDay.day.maxtemp_c)}
                     ></SingleWeather>;
                 })
             }
@@ -37,9 +43,18 @@ const Container = styled.div`
   color: white;
   height: 100vh;
   width: 100vw;
-  padding: 5rem 6rem;
-  `;
-
+  padding: 0em 6rem;
+`;
+const ShowCity = styled.div`
+    text-align: center;
+    margin-top: 50px;
+    h2{
+        padding-bottom: 30px;
+    }
+    h1{
+        font-size: 2em;
+    }
+`;
 const Display = styled.div`
     display: flex;
     justify-content: center;
