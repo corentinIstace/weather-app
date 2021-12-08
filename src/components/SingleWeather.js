6ec38caefa737fc09ebdc0197caaa9624cf8b6c3
corentinIstace <corentinistace1@gmail.com>
2021-12-08 16:19:02 +0100

    adding day for each card, design background of the card hot/cold + small modification

--- src/components/SingleWeather.js
+++ src/components/SingleWeather.js
@@ -1,21 +1,16 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
+import cold from '../img/cold.jpg';
+import hot from '../img/sun.jpg';
 
-const SingleWeather = ({date, condition, minTemp, maxTemp}) => {
-    const getDay = (jj) => {
-        let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-
-        let day = days[jj.getDay()];
-
-        return `${day}`;
-    }
+const SingleWeather = ({date, condition, minTemp, maxTemp, day}) => {
     return (
             <Card>
                 <CardDate>
-                    <h2>{getDay(new Date())},</h2>
+                    <h2>{day},</h2>
                     <h2>{date}</h2>
                 </CardDate>
-                <CardImg>
+                <CardImg minTemp={minTemp}>
                     <img src={condition.icon} alt={condition.text} />
                     <h3>{condition.text}</h3>
                 </CardImg>
@@ -31,39 +26,55 @@ const Card = styled.section`
     display: flex;
     flex: 0 1 250px;
     flex-direction: column; 
-    border: 2px solid grey;
     border-radius: 10px;
     height: 350px;
     margin: 0rem 2rem;
+    margin-bottom: 4rem;
+    /* padding: 2rem; */
+    box-shadow: 8px 10px 10px rgb(0 0 0 / 30%);
     &:hover{
-    transform: scale3d(1.1, 1.1, 1.1);
+        transform: scale3d(1.1, 1.1, 1.1);
+        box-shadow:  0 0 3em rgb(0 0 0 / 30%);
+        transition: 0.5s ease;
 }
 `;
 const CardDate = styled.div`
-    background-color: blue;
     text-align: center;
-    background-color: #343d46;
+    background-color: rgba(52,61,70, 0.4) ;
     border-top-left-radius: 10px;
     border-top-right-radius: 10px;
+    padding: 1rem;
     flex: 1;
     h2{
         padding: 5px;
     }
-`;
+    `;
+
 const CardImg = styled.div`
-    background-color: #4f5b66;
+    background: ${({minTemp})=> {
+        return minTemp < 23 ? `url(${cold})` : `url(${hot})`}
+    };     
+    background-position: bottom;
     text-align: center;
     padding-top: 10px;
     flex: 2;
-
+    p{
+     padding-top: 20px;
+    }
+    img{
+      padding-top: 30px;
+    }
 `;
+
 const CardTemp = styled.div`
-    display: flex;
-    justify-content: space-evenly;
-    background-color: #65737e;
+    background-color: rgba(52,61,70, 0.4) ;
     border-bottom-left-radius: 10px;
     border-bottom-right-radius: 10px;
+    padding: 1rem;
     padding-top: 10px;
+    display: flex;
+    justify-content: space-evenly;
     flex: 0.7;
 `;
+
 export default SingleWeather
